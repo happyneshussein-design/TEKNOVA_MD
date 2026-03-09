69fcf1fe2ccfe71858b71e7c03e705bf163e6d9b
happyneshussein-design <happyneshussein@gmail.com>
2026-03-09 10:06:10 +0300

    Implement profile command with bot details
    Added profile command to send bot information with an image.

--- plugins/profile.js
+++ plugins/profile.js
@@ -1,1 +1,24 @@
+export const command = ["profile","bot"]
 
+export default async function(sock,msg,args,config){
+
+const from = msg.key.remoteJid
+
+await sock.sendMessage(from,{ 
+image: { url: "./assets/teknova.jpg" },
+caption:`🤖 *${config.botname}*
+
+👑 OWNER: ${config.owner}
+📱 NUMBER: ${config.ownerNumber}
+🌐 WEBSITE:
+${config.website}
+
+📢 Follow the TEKNOVA-MD channel:
+${config.channel}
+
+⚡ Ultra WhatsApp Bot
+🚀 AI • Downloader • Games • Tools • Group Management`
+
+})
+
+}
