fca219949aa6614a50978fad046cedd933d6b723
happyneshussein-design <happyneshussein@gmail.com>
2026-03-09 09:24:14 +0300

    config.js

---
+++ config.js
@@ -0,0 +1,18 @@
+export const config = {
+
+botname: "TEKNOVA MD ULTRA",
+owner: "SmartWave Tech",
+
+// owner WhatsApp number
+ownerNumber: "255611378027",
+
+// command prefix
+prefix: ".",
+
+// channel link
+channel: "https://whatsapp.com/channel/0029VbCJScD72WTnnyeALu2h",
+
+// official website
+website: "https://v0-teknova-website-design.vercel.app/",
+
+}
