308db7df0d6abef322cab6f65e0bde1f054d8559
happyneshussein-design <happyneshussein@gmail.com>
2026-03-09 09:51:39 +0300

    Add ban and unban functionalit
    Implement ban and unban commands with JSON storage.

--- plugins/ban.js
+++ plugins/ban.js
@@ -1,1 +1,22 @@
+import fs from "fs"
 
+export const command = ["ban","unban"]
+
+export default async function(sock,msg,args){
+
+const from = msg.key.remoteJid
+const user = msg.key.participant || msg.key.remoteJid
+
+let data = JSON.parse(fs.readFileSync("./database/ban.json"))
+
+if(args[0] === "ban"){
+
+if(!data.includes(user)) data.push(user)
+
+fs.writeFileSync("./database/ban.json",JSON.stringify(data))
+
+sock.sendMessage(from,{text:"🚫 User banned"})
+
+}
+
+}
