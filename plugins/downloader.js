00ef10410196b4fbf82347936d964ac4a05738c6
happyneshussein-design <happyneshussein@gmail.com>
2026-03-09 09:59:58 +0300

    Implement downloader in downloader.js
    Added downloader functionality for various commands.

--- plugins/downloader.js
+++ plugins/downloader.js
@@ -1,1 +1,18 @@
 
+import axios from "axios"
+
+export const command = ["yt","play","tiktok","fb","insta"]
+
+export default async function(sock,msg,args){
+
+const from = msg.key.remoteJid
+
+if(!args[0]) return sock.sendMessage(from,{text:"❗ Weka link baada ya command"})
+
+sock.sendMessage(from,{text:"⏳ Inajaribu kudownload..."})
+
+// mfano tu (unaweza badili API)
+sock.sendMessage(from,{text:`✅ Link imepokelewa: ${args[0]}
+Downloader itafanyiwa kazi.`})
+
+}
