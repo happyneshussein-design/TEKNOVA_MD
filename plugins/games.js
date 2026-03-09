8f51fc2e6d4a51dcb1471250c2f38150578428b2
happyneshussein-design <happyneshussein@gmail.com>
2026-03-09 10:03:54 +0300

    Add game commands and default export function

--- plugins/games.js
+++ plugins/games.js
@@ -1,1 +1,9 @@
+export const command = ["truth","dare","quiz","math"]
 
+export default async function(sock,msg,args){
+
+const from = msg.key.remoteJid
+
+sock.sendMessage(from,{text:"🎮 Game imeanza!"})
+
+}
