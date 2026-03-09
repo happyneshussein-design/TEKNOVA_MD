1c15a230dcd0a50281f4f26bc0989b9adbc404c9
happyneshussein-design <happyneshussein@gmail.com>
2026-03-09 10:02:06 +0300

    Add tools command with default export function

--- plugins/tools.js
+++ plugins/tools.js
@@ -1,1 +1,9 @@
+export const command = ["qr","translate","weather"]
 
+export default async function(sock,msg,args){
+
+const from = msg.key.remoteJid
+
+sock.sendMessage(from,{text:"🛠️ Tools command imefanya kazi"})
+
+}
