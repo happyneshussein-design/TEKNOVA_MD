982966ecc3184cb0032a5d2898080ba86a7599da
happyneshussein-design <happyneshussein@gmail.com>
2026-03-09 10:01:02 +0300

    Add sticker command and message prompt

--- plugins/sticker.js
+++ plugins/sticker.js
@@ -1,1 +1,9 @@
+export const command = ["sticker","s"]
 
+export default async function(sock,msg){
+
+const from = msg.key.remoteJid
+
+sock.sendMessage(from,{text:"🖼️ Tuma picha au video fupi kisha reply na .sticker"})
+
+}
